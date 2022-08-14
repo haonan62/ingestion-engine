006d46a8c84efda82b49af8c2be94120601f4a19
haonanxu <haonan.xu@singtel.com>
2022-08-14 15:58:21 +0800

    intial setup

--- src/main/java/com/xuliuxianglab/forex/portal/ingestion/engine/Application.java
+++ src/main/java/com/xuliuxianglab/forex/portal/ingestion/engine/Application.java
@@ -2,7 +2,11 @@ package com.xuliuxianglab.forex.portal.ingestion.engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients("com.xuliuxianglab.forex.portal.ingestion.engine")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
